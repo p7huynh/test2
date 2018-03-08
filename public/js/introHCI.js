3062f3eb9b82017d49a9373fde4a52dd2d01196d
Phan Huynh <p7huynh@ucsd.edu>
2018-03-07 17:05:32 -0800

    buttonot

---
+++ public/js/introHCI.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+
+ //Function that is called when the document is ready.
+ 
+function initializePage() {
+	// your code here
+	$(".TopBtn").click(function() {
+		//gtag("create","UA-115310420-1","auto");
+		// ga("send", "event", "top", "click");
+			gtag('create','UA-115310420-1','auto');
+			ga('send','event','top','click');
+			console.log("Event loaded");
+	});
+}
+
+// /*
+//  * Function that is called when the document is ready.
+//  */
+// function initializePage() {
+// 	// your code here
+// 	$(".TopBtn").click(clickTopBtn);
+// };
+
+// function clickTopBtn(){
+// 	e.preventDefault();
+// 	gtag("create","UA-115310420-1","auto");
+// 	gtag("send","event","top","click");
+// };
